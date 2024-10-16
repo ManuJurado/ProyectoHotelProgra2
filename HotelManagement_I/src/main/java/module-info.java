4a5a8fb43a023567d9429cc79b5f8f535567e094
Manuel Sosa Jurado <113263399+ManuJurado@users.noreply.github.com>
2024-10-16 14:35:01 -0300

    Add files via upload
    Primer commit de version inicial del ui. Sin gestion de usuarios, solamente mostrando funcionamiento inicial de JavaFX.

---
+++ HotelManagement_I/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module hotel_management_ui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javafx.media;
+
+    opens hotel_management_ui to javafx.fxml;
+    exports hotel_management_ui;
+}
